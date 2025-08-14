4d1bfbcc933ef5a0874fd6676798baa40b455a93
ndizeyedavid <davidndizeye101@gmail.com>
2025-08-14 19:47:06 +0200

    [ADD] page loading with NProgress

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -6,36 +6,49 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import pb from "@/lib/pb";
 import { Toaster } from "sonner";
+import { usePathname } from "next/navigation";
+// @ts-ignore
+import NProgress from "nprogress";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const router = useRouter();
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
 
-    useEffect(() => {
-        if (!pb.authStore.isValid) return router.replace("/auth/login");
-    }, []);
+  useEffect(() => {
+    if (!pb.authStore.isValid) return router.replace("/auth/login");
+  }, []);
 
-    return (
-        <>
-            <Toaster richColors position="top-right" />
+  useEffect(() => {
+    NProgress.start();
+    NProgress.done();
+  }, [pathname]);
 
-            <SidebarProvider
-                style={
-                    {
-                        "--sidebar-width": "calc(var(--spacing) * 72)",
-                        "--header-height": "calc(var(--spacing) * 12)",
-                    } as React.CSSProperties
-                }
-            >
-                <AppSidebar variant="inset" id="no-print" />
-                <SidebarInset>
-                    <SiteHeader />
-                    <div className="flex flex-1 flex-col">
-                        <div className="@container/main flex flex-1 flex-col gap-2">
-                            {children}
-                        </div>
-                    </div>
-                </SidebarInset>
-            </SidebarProvider>
-        </>
-    );
+  return (
+    <>
+      <Toaster richColors position="top-right" />
+
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" id="no-print" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              {children}
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
 }
