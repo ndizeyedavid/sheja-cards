2fe41643fb76b7a0238527b9996b491f63fb2576
David Ndizeye <davidndizeye101@gmail.com>
2025-08-16 05:08:27 +0200

    Delete old-backend directory

--- backend/src/types/staff.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IStaff {
-  schoolId: string;
-  name: string;
-  email: string;
-  phone: string;
-  idNumber: string;
-  role: "HEADMASTER" | "BURSAR" | "PATRON" | "DISCIPLINE_MASTER" | "DOS";
-  password: string;
-}
